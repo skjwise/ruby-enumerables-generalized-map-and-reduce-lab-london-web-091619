8bf7d58a93cafe99049a19d5ca77a743368d925a
Sarah Wise <skjwise@hotmail.com>
2019-08-29 15:24:30 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
 # Your Code Here
+def map(arr, &block)
+  arr.map(&block)
+end
+  
+def reduce(array, start_point = nil, &block)
+  if start_point.nil?
+    array.reduce(&block)
+  else
+    array.reduce(start_point, &block)
+  end
+end
+  
